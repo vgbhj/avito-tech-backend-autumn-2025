5297d4d33f4e9f133ec412e177167a89590fd459
Mikhail Parfenov <vgbhjob@gmail.com>
2025-11-23 19:41:57 +0300

    feat(delivery): add team HTTP handlers

---
+++ internal/delivery/http/handlers/team_handler.go
@@ -0,0 +1,67 @@
+package handlers
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+
+	"github.com/avito-tech-backend-autumn-2025/internal/delivery/http/dto"
+	"github.com/avito-tech-backend-autumn-2025/internal/usecase/team"
+)
+
+type TeamHandler struct {
+	createTeamUseCase *team.CreateTeamUseCase
+	getTeamUseCase    *team.GetTeamUseCase
+}
+
+func NewTeamHandler(
+	createTeamUseCase *team.CreateTeamUseCase,
+	getTeamUseCase *team.GetTeamUseCase,
+) *TeamHandler {
+	return &TeamHandler{
+		createTeamUseCase: createTeamUseCase,
+		getTeamUseCase:    getTeamUseCase,
+	}
+}
+
+func (h *TeamHandler) CreateTeam(c *gin.Context) {
+	var req dto.CreateTeamRequest
+	if err := c.ShouldBindJSON(&req); err != nil {
+		respondError(c, http.StatusBadRequest, "INVALID_REQUEST", "invalid request body")
+		return
+	}
+
+	useCaseReq := dto.ToCreateTeamRequest(req)
+	team, err := h.createTeamUseCase.Execute(useCaseReq)
+	if err != nil {
+		handleDomainError(c, err)
+		return
+	}
+
+	response := dto.TeamResponse{
+		Team: dto.ToTeamDTO(team),
+	}
+
+	respondJSON(c, http.StatusCreated, response)
+}
+
+func (h *TeamHandler) GetTeam(c *gin.Context) {
+	teamName := c.Query("team_name")
+	if teamName == "" {
+		respondError(c, http.StatusBadRequest, "INVALID_REQUEST", "team_name is required")
+		return
+	}
+
+	team, err := h.getTeamUseCase.Execute(teamName)
+	if err != nil {
+		handleDomainError(c, err)
+		return
+	}
+
+	respondJSON(c, http.StatusOK, dto.ToTeamDTO(team))
+}
+
+func (h *TeamHandler) RegisterRoutes(r *gin.Engine) {
+	r.POST("/team/add", h.CreateTeam)
+	r.GET("/team/get", h.GetTeam)
+}
